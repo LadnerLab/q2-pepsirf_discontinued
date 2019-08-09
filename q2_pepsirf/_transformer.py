fc21c8e685e8644c824c1fce20b967dd9c54e5b6
Bob Costas <finkzane@gmail.com>
2019-08-09 14:54:49 -0700

    added _fasta_to_series transformer

--- q2_pepsirf/_transformer.py
+++ q2_pepsirf/_transformer.py
@@ -1,9 +1,29 @@
 from .plugin_setup import plugin
+import skbio
+from q2_types.feature_data import FeatureData
 from ._format import LinkedSpeciesPeptideFmt
 from ._format import SequenceNamesFmt
-import pandaas as pd
+from ._format import ProteinSequenceFmt
+import pandas as pd
 
 
+def _read_fasta( path ):
+    return skbio.read( path, format = 'fasta' )
+
+def _fasta_to_series( ff ):
+    data = {}
+
+    for sequence in _read_fasta( str( ff ) ):
+        id_ = sequence.Metadata[ 'id' ]
+
+        if id_ in data:
+            raise ValueError( 'FASTA Sequence names must be unique. The '
+                              'following ID was included more than once: %s.'
+                              % id_
+                            )
+        data[ id_ ] = sequence
+    return pd.Series( data )
+    
 def _file_to_df( ff, index_col = 'Name' ):
     df = pd.read_csv( str( ff ), sep = '\t',
                       index_col = index_col
@@ -20,4 +40,8 @@ def _2( ff: LinkedSpeciesPeptideFmt ) -> pd.DataFrame:
 
 @plugin.register_transformer
 def _3( ff: SequenceNamesFmt ) -> pd.DataFrame:
+    return _file_to_df( ff, index_col = 'Name' )
+
+@plugin.register_transformer
+def _4( ff: ProteinSequenceFmt ) -> pd.Series:
     return _file_to_df( ff, index_col = 'Name' )
