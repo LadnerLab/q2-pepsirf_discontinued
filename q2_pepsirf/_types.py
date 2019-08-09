3dece672d883e93a6dd95bc31ceec0528ac44efb
Bob Costas <finkzane@gmail.com>
2019-08-09 14:55:02 -0700

    added ProteinSequence semantic type

--- q2_pepsirf/_types.py
+++ q2_pepsirf/_types.py
@@ -1,4 +1,8 @@
 from qiime2.plugin import SemanticType
+from q2_types.feature_data import FeatureData
 
 LinkedSpeciesPeptide = SemanticType( 'LinkedSpeciesPeptide' )
 SequenceNames = SemanticType( 'SequenceNames' )
+ProteinSequence = SemanticType( 'ProteinSequence',
+                                variant_of = FeatureData.field[ 'type']
+                              )
