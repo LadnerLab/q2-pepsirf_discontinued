807d3a965a4a9067b5fd9697a7fd80afd65ea3c2
Bob Costas <finkzane@gmail.com>
2019-08-09 13:16:58 -0700

    added SequenceNamesFmt/DirFmt

--- q2_pepsirf/_format.py
+++ q2_pepsirf/_format.py
@@ -4,7 +4,6 @@ import qiime2.plugin.model as model
 import numpy as np
 from qiime2.plugin import ValidationError
 
-
 class LinkedSpeciesPeptideFmt( model.TextFileFormat ):
     def _check_n_records( self, n ):
         with open( str( self ) ) as fh:
@@ -27,3 +26,24 @@ LinkedSpeciesPeptideDirFmt = model.SingleFileDirectoryFormat( 'LinkedSpeciesPept
                                                               'linked.tsv',
                                                               LinkedSpeciesPeptideFmt
                                                             )
+
+class SequenceNamesFmt( model.TextFileFormat ):
+    def _check_n_records( self, n ):
+        with open( str( self ) ) as fh:
+            for lineno, record in enumerate( fh ):
+                if lineno == n:
+                    break
+                else:
+                    if len( record.strip() ) == 0:
+                        raise ValidationError( 'Error on line %d, '
+                                               'empty lines are not permitted.' % ( lineno + 1 )
+                                             )
+
+    def _validate_( self, level ):
+        record_count_map = { 'min': 5, 'max': np.inf }
+        self._check_n_records( record_count_map[ level ] )
+
+SequenceNamesDirFmt = model.SingleFileDirectoryFormat( 'SequenceNamesDirFmt',
+                                                       'names.txt',
+                                                       SequenceNamesFmt
+                                                     )
