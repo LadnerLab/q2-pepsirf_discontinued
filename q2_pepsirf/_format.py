8711ef96fcc32f4ac54984cbb5791290ee241351
Bob Costas <finkzane@gmail.com>
2019-08-09 12:29:17 -0700

    added LinkedSpeciesPeptideFmt, DirFmt

--- q2_pepsirf/_format.py
+++ q2_pepsirf/_format.py
@@ -1,4 +1,29 @@
+import csv
 
 import qiime2.plugin.model as model
 import numpy as np
 from qiime2.plugin import ValidationError
+
+
+class LinkedSpeciesPeptideFmt( model.TextFileFormat ):
+    def _check_n_records( self, n ):
+        with open( str( self ) ) as fh:
+            csv_reader = csv.reader( fh, delimiter = '\t' )
+
+            for i, row in enumerate( csv_reader ):
+                if i == n:
+                    break
+                else:
+                    if len( row ) != 2:
+                        raise ValidationError( 'Incorrect number of fields detected on line %d. '
+                                               'Should be exactly 2.' % ( i + 1 )
+                                             )
+
+    def _validate_( self, level ):
+        record_count_map = { 'min': 5, 'max': np.inf }
+        self._check_n_records( record_count_map[ level ] )
+
+LinkedSpeciesPeptideDirFmt = model.SingleFileDirectoryFormat( 'LinkedSpeciesPeptideDirFmt',
+                                                              'linked.tsv',
+                                                              LinkedSpeciesPeptideFmt
+                                                            )
