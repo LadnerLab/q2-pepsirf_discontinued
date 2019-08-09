6da179c0ae2fcd368c38fc2aa0b919d0d4e5b030
Bob Costas <finkzane@gmail.com>
2019-08-09 14:54:37 -0700

    added ProteinSequenceFmt

--- q2_pepsirf/_format.py
+++ q2_pepsirf/_format.py
@@ -6,7 +6,7 @@ from qiime2.plugin import ValidationError
 
 class LinkedSpeciesPeptideFmt( model.TextFileFormat ):
     def _check_n_records( self, n ):
-        with open( str( self ) ) as fh:
+        with open( str( self ), 'r' ) as fh:
             csv_reader = csv.reader( fh, delimiter = '\t' )
 
             for i, row in enumerate( csv_reader ):
@@ -29,7 +29,7 @@ LinkedSpeciesPeptideDirFmt = model.SingleFileDirectoryFormat( 'LinkedSpeciesPept
 
 class SequenceNamesFmt( model.TextFileFormat ):
     def _check_n_records( self, n ):
-        with open( str( self ) ) as fh:
+        with open( str( self ), 'r' ) as fh:
             for lineno, record in enumerate( fh ):
                 if lineno == n:
                     break
@@ -47,3 +47,38 @@ SequenceNamesDirFmt = model.SingleFileDirectoryFormat( 'SequenceNamesDirFmt',
                                                        'names.txt',
                                                        SequenceNamesFmt
                                                      )
+
+class ProteinSequenceFmt( model.TextFileFormat ):
+    def _check_n_records( self, n ):
+        last_was_id = False
+        non_id_lines = 0
+
+        with open( str( self ), 'r'  ) as fh:
+            for lineno, line in enumerate( fh ):
+                if line[ 0 ] == '>':
+                    if last_was_id:
+                        raise ValueError( 'Two sequence names were found in a row, '
+                                          'error on line %d.' % ( lineno + 1 )
+                                        )
+                    last_was_id = True
+                else:
+                    last_was_id = False
+                    non_id_lines += 1
+                        
+                if lineno >= n:
+                    if non_id_lines: # there is at least one name and one sequence
+                        break
+                    raise ValueError( 'Fasta File does not contain at least one '
+                                      'name and one sequence.'
+                                    )
+
+                    
+
+    def _validate_( self, level ):
+        record_count_map = { 'min': 5, 'max': np.inf }
+        self._check_n_records( record_count_map[ level ] )
+        
+ProteinSequenceDirFmt = model.SingleFileDirectoryFormat( 'ProteinSequenceDirFmt',
+                                                         'sequences.fasta',
+                                                         ProteinSequenceFmt
+                                                       )
